985a50bce076b7f18ca3e2e733c1f3aa7f9d5b27
mallyk <kyle.mally@gmail.com>
2017-12-05 15:03:19 -0600

    lesson 6, section 55

--- src/person.js
+++
@@ -1,7 +0,0 @@
-console.log('person.js is running');
-
-export const isAdult = (age) => age >= 18; 
-export const canDrink = (age) => age >= 21;
-const isSenior = (age) => age >= 65;
-
-export default isSenior;
